f8eeba06bc3177a644c86ad404683da06f0b7112
mekhti11 <mekhti11@gmail.con>
2020-09-25 17:12:04 +0300

    Backend

---
+++ template.js
@@ -0,0 +1,13 @@
+export default () => { 
+    return `<!doctype html>
+        <html lang="en"> 
+            <head>
+            <meta charset="utf-8">
+            <title>MERN Kickstart</title> 
+            </head>
+            <body>
+                <div id="root"></div>
+                <h1>Hello World</h1>
+            </body>
+        </html>` 
+}
